1834e0dc0b405ec4fd5e9b86a442d603b786752b
Quan Tran <minhquantran451@gmail.com>
2023-01-24 17:43:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ISS Overhead Notifier
+
+The program check if the International Space Station is floating above your head by using Open Notify API.
+
+## How to use
+
+First clone the repository and install all the required modules. Then run the following command:
+
+```
+python main.py
+```
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## License
+
+[MIT](LICENSE)
---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+from datetime import datetime
+
+# Feel free to change the latitude and longitude coordinates to fit your place.
+# In my case, they are for Austin, Texas.
+
+MY_LAT = 30.266666
+MY_LONG = -97.733330
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+    return False
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+    return False
+
+if is_iss_overhead() and is_night():
+    print("Look up")
+else:
+    print("Nevermind")
+
+
